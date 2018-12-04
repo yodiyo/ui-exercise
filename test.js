864629a74745e5029a46ce2ed50babc0356d33b1
Yorick <info@theyoricktouch.com>
2018-12-04 23:03:40 +0000

    add cards, thumbnails and font

---
+++ test.js
@@ -0,0 +1,10 @@
+// enable use of trial font from myfonts.com
+
+(function() {
+    var trial = document.createElement('script');
+    trial.type = 'text/javascript';
+    trial.async = true;
+    trial.src = 'https://easy.myfonts.net/v2/js?sid=112449(font-family=Charter+BT+Pro+Bold)&sid=112451(font-family=Charter+BT+Pro+Roman)&key=696Dp8JIze';
+    // var head = document.getElementsByTagName("head")[0];
+    // head.appendChild(trial);
+})();
